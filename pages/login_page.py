960d3947c9a6fa1e62fe77e113ca736b6868cae7
Barmagload <ds_anisimov@mail.ru>
2020-09-03 16:19:08 +0300

    UPDATE Little update names and url in login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -9,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         print(self.browser.current_url)
-        assert self.browser.current_url == LoginPageLocators.LOGIN_LINK, "Actual login link is incorrect"
+        assert self.browser.current_url == LoginPageLocators.LOGINPAGE_LINK, "Actual login link is incorrect"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
