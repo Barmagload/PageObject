1e9fb9a10633ac7d5168d94f6116ed246a5f1472
Barmagload <ds_anisimov@mail.ru>
2020-09-03 16:18:25 +0300

    Little update names and url in login_page

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,6 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOGIN_LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    LOGINPAGE_LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
