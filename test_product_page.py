2b539ff8beb7cc46fabcb8d6a048dca3d6b28d0e
Barmagload <ds_anisimov@mail.ru>
2020-09-07 12:49:14 +0300

    UPDATE Transfer "go_to_login_page()" and "should_be_login_link()" to base_page.py, def sort

--- test_product_page.py
+++ test_product_page.py
@@ -1,19 +1,11 @@
 from .pages.product_page import ProductPage
 import pytest
 
-"""
-@pytest.mark.parametrize('link',
-                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                              pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+
+@pytest.mark.parametrize('num', ["0", "1", "2", "3", "4", "5", "6",
+                                 pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
@@ -41,7 +33,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.should_dissapear_of_success_message()
-"""
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
