fcf7787e719cfac2f32ee5e0660a46671fbf884f
Barmagload <ds_anisimov@mail.ru>
2020-09-08 17:27:43 +0300

    Add TestUserAddToBasketFromProductPage as fixture (def setup) + update requirements.txt

--- pages/locators.py
+++ pages/locators.py
@@ -5,7 +5,7 @@ class BasePageLocators():
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini [href='/en-gb/basket/']")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BOOK_IN_BASKET = (By.CSS_SELECTOR, ".basket-items")
@@ -15,7 +15,11 @@ class BasketPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGINPAGE_LINK = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name = 'registration_submit']")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
 
 
 class MainPageLocators():
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,35 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import faker
 import pytest
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        f = faker.Faker()
+        email = f.email()
+        password = "qweqweqweqwe"
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_be_equal_book_names_and_book_prices()
 
 
 @pytest.mark.parametrize('num', ["0", "1", "2", "3", "4", "5", "6",
